ea2f662c812497f89af7a70dbf9c062e91c1ffec
suhaib079 <suhaibabdennabi33@gmail.com>
2021-08-02 17:43:16 +0300

    d

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <footer  className = "p-5 text-left bg-green-500 h-15">
+        <footer  className = "p-5 text-left bg-green-500 h-15" className ="">
             2021 &copy;
         </footer>
     )
--- components/main.js
+++ components/main.js
@@ -1,58 +1,66 @@
-import React, {useState} from "react";
-import "tailwindcss/tailwind.css"
+import Head from 'next/head'
+import { useState } from 'react'
 
-export default function CookieStandForm(){
-const [store, setStore] = useState('')
-    
-    function createHandler(event){
-        event.preventDefault();
-        let store = {
-            location: event.target.location.value,
-            min: event.target.min.value,
-            max: event.target.max.value,
-            ave: event.target.ave.value,
-        }
-        setStore(store)
+export default function Home() {
+
+  const [store,setStore] = useState('');
+
+  function handler(e){
+    e.preventDefault();
+
+    let store = {
+      location: e.target.location.value,
+      min: e.target.min.value,
+      max: e.target.max.value,
+      avg: e.target.avg.value,
     }
+    setStore(store)
+  }
+  
+  return (
+    <div >
+      <Head>
+        <title>Cookie Stand Admin</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className='p-5 bg-green-400'>
+        <h1 className='text-4xl' >Cookie Stand Admin</h1>
+      </header>
 
 
+      <main className='grid w-full p-10 px-0 text-center bg-green-200 justify-items-stretch'>
+        <h1 className = "p-5 text-2xl">Create Cookie Stand</h1>
+      
+      <form onSubmit = {handler}>
+        <label for='loc' className = "inline-block m-3" >Location</label>
+        <input name="location" type='text'></input>
+        <label className = "inline-block m-3" for='loc'>Minimum Customers Per Hour</label>
+        <input name="min" type='number'></input>
+        <label for='loc' className = "inline-block m-3">Maximum Customers Per Hour</label>
+        <input name="max" type='number'></input>
+        <label for='loc' className = "inline-block m-3">Average Cookies per Sale</label>
+        <input name="avg" type='number'></input>
+        <button className = "p-8 py-5 m-3 bg-green-500" type = "submit">Create</button>
 
-    return (
-        <main className = "grid w-full p-10 px-0 text-center bg-green-100 justify-items-stretch">
-        <form onSubmit = {CookieStandForm} className = "px-10 py-5 mx-12 bg-green-300 rounded w-4/5justify-self-center">
-            <h2 className = "p-5 text-2xl">
-                Create Cookie Stand
-            </h2>
-            <div>
-                <label className = "w-5/6">
-                    Location
-                </label>
-                <input type = "text" placeholder="Cookie Stand Location"/>
-            </div>
-            <div>
-                <label className = "inline-block m-3">
-                    Minimum Customers Per Hour
-                    </label>
-                <input type = "number"/>
-
-                <label className = "inline-block m-3">
-                    Maximum Customers Per Hour
-                    </label>
-                <input type = "number"/>
-
-                <label className = "inline-block m-3">
-                    Average Cookies per Sale
-                    </label>
-                    <input  type="number"/>
-
-                <button className = "p-8 py-5 m-3 bg-green-400" type = "submit">Create</button>
-                    
-            </div>
-        </form>
-        <section className="text=center p-10">
-        <p className='m-5 text-gray-600'>Report Table coming soon .....</p>
+        <p className='m-5 text-gray-800'>Report Table coming soon .....</p>
         <p className="m-3">{JSON.stringify(store)}</p>
-        </section>
-        </main>
-    )
+
+        
+        
+        
+      </form>
+
+
+
+
+      </main>  
+
+      <footer className='p-3 bg-green-400'>
+
+      &copy;2021  
+
+      </footer>
+    </div>
+  )
 }
